fa13d7170ef26720b9c9eaf767a97df7f86a6332
Geng Cha Vue <51110353+gvue003@users.noreply.github.com>
2023-08-23 00:16:55 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
+// Declare variables
 let song;
 let fft;
 let particles = [];
 
+// Preload the song before setup
 function preload() {
   song = loadSound("Confetti - Hot.mp3");
 }
 
 function setup() {
+  // Create canvas and set angle mode
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+  
+  // Initialize FFT analyzer
   fft = new p5.FFT();
 }
 
@@ -18,26 +23,30 @@ function draw() {
   strokeWeight(3);
   noFill();
 
+  // Translate origin to center of canvas
   translate(width / 2, height / 2);
 
+  // Analyze audio and get amplitude
   fft.analyze();
-  amp = fft.getEnergy(20, 200);
+  let amp = fft.getEnergy(20, 200);
 
+  // Get waveform data
   let wave = fft.waveform();
 
+  // Draw the waves
   for (let t = -1; t <= 1; t += 2) {
     beginShape();
     for (let i = 0; i <= 180; i += 0.5) {
       let index = floor(map(i, 0, 180, width, 0, wave.length - 1));
-
       let r = map(wave[index], -1, 1, 150, 350);
-
       let x = r * sin(i) * t;
       let y = r * cos(i);
       vertex(x, y);
     }
     endShape();
   }
+
+  // Create and manage particles
   let p = new Particle();
   particles.push(p);
 
@@ -51,6 +60,7 @@ function draw() {
   }
 }
 
+// Handle mouse click for audio control
 function mouseClicked() {
   if (song.isPlaying()) {
     song.pause();
@@ -61,16 +71,16 @@ function mouseClicked() {
   }
 }
 
+// Particle class
 class Particle {
   constructor() {
     this.pos = p5.Vector.random2D().mult(250);
     this.vel = createVector(0, 0);
     this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
-
     this.w = random(3, 5);
-
-    (this.color = [random(200, 255)]), [random(200, 255)], [random(200, 255)];
+    this.color = [random(200, 255), random(200, 255), random(200, 255)];
   }
+
   update(cond) {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
@@ -80,18 +90,16 @@ class Particle {
       this.pos.add(this.vel);
     }
   }
+
   edges() {
-    if (
+    return (
       this.pos.x < -width / 2 ||
-      this.pos.y > width / 2 ||
+      this.pos.x > width / 2 ||
       this.pos.y < -height / 2 ||
       this.pos.y > height / 2
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+    );
   }
+
   show() {
     noStroke();
     fill(this.color);
